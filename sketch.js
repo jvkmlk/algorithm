923917cc0e2c3750498da054cdf15b13ddf5b408
jvkmlk <72926365+jvkmlk@users.noreply.github.com>
2020-10-25 19:32:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var movingrect,fixedrect;
+
+function setup() {
+  createCanvas(1200,800);
+  movingrect=createSprite(300, 200, 50, 50);
+  movingrect.shapeColor="green";
+  fixedrect=createSprite(200, 200, 50, 50);
+  fixedrect.shapeColor="green";
+}
+
+function draw() {
+  background(0,0,0);
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+  if (movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2&&
+    fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2&&
+    movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2&&
+    fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2){
+    movingrect.shapeColor="red";
+    fixedrect.shapeColor="red";
+  }
+
+ else {
+   movingrect.shapeColor="green";
+   fixedrect.shapeColor="green";
+ } 
+  drawSprites();
+}
